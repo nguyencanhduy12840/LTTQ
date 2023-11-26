6f33dab871cedc67d8d2348c627bc4bcbec836e6
nguyen canh duy <huyduynguyen12@gmail.com>
2023-11-26 17:21:42 +0700

    add lttq

---
+++ Bai01/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            lblChar.Text = "Key Pressed: " + e.KeyChar;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            lblKeyInfo.Text = "Alt: " + (e.Alt ? "Yes" : "No") + "\n" +
+                "Shift: " + (e.Shift ? "Yes" : "No") + "\n" +
+                "Ctrl: " + (e.Control ? "Yes" : "No") + "\n" +
+                "KeyCode: " + e.KeyCode + "\n" +
+                "KeyData: " + e.KeyData + "\n" +
+                "KeyValue: " + e.KeyValue + "\n";
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Toa do:(" + e.Location.X.ToString() + "," + e.Location.Y.ToString() + ")");
+        }
+    }
+}
---
+++ Bai03/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Bai03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Ban co chac muon thoat khong?", "Xac nhan", MessageBoxButtons.YesNo);
+            if(result == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Hom nay la ngay " + DateTime.Now.ToString("dd/MM/yyyy") + " - Bay gio la " + DateTime.Now.ToString("hh:mm:ss");
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Media File|*.avi;*.mpeg;*.wav;*.midi;*.mp4;*.mp3";
+            openFileDialog1.Title = "Select a Media File";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                axWindowsMediaPlayer1.URL = openFileDialog1.FileName;
+            }
+        }
+    }
+}
